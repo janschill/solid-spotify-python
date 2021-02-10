211311877706e8f2f69eb8158b16f2bc379ba39e
Jan Schill <janschill@me.com>
2021-02-10 15:27:33 +0100

    WIP app

--- solidspotify/web/app.py
+++ solidspotify/web/app.py
@@ -1,12 +1,33 @@
+import pprint
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import CSVW, DC, DCAT, DCTERMS, DOAP, FOAF, ODRL2, ORG, OWL, PROF, PROV, RDF, RDFS, SDO, SH, SKOS, SOSA, SSN, TIME, VOID, XMLNS, XSD
+
 from flask import Flask, render_template
 from solidspotify.util.enums.http import HTTPMethod
 
 from solidspotify.modules.solid.client import client
-response = client.fetch('http://localhost:3000')
-print(response)
+tracks = client.fetch('http://localhost:3000/solid-spotify/recent_tracks.ttl')
+# print(tracks)
 
-put_response = client.fetch('http://localhost:3000/test/test.ttl',
-                            method=HTTPMethod.PUT, body='<ex:s> <ex:p> <ex:o>.')
+
+g = Graph()
+g.parse('http://localhost:3000/solid-spotify/recent_tracks.ttl', format="turtle")
+# print(g.serialize(format="turtle").decode("utf-8"))
+# 1. Find all RDF.type = SDO.MusicRecording O(N)
+# 2. Get for each all their triples 0(N)
+#
+#
+#
+#
+for s, p, o in g.triples((None, RDF.type, SDO.MusicRecording)):
+    print("{} is a track".format(s))
+
+# for stmt in g:
+#     pprint.pprint(stmt)
+
+
+# put_response = client.fetch('http://localhost:3000/test/test.ttl',
+# method = HTTPMethod.PUT, body = '<ex:s> <ex:p> <ex:o>.')
 
 app = Flask(__name__)
 
